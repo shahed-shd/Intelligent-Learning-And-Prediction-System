1a4946582d84338031c123a557d8c5d13eb26a20
Md. Shahedul Islam Shahed <shahed.shd777@gmail.com>
2018-09-19 02:00:26 +0600

    global constant values are stored here

---
+++ model/globalvalues.py
@@ -0,0 +1,13 @@
+# Values in this file are manually set.
+
+import os
+
+
+class GlobalValues(object):
+    def __init__(self, **kwargs):
+        self.app_dir = '/home/shahed/ShdHomeData/ILPS'
+
+
+    def get_persistent_data_file_path(self):
+        p = 'data/persistent_data.json'
+        return os.path.join(self.app_dir, p)
