c283351338ea4f84e6c88861a8bdb94089205cfb
Md. Shahedul Islam Shahed <shahed.shd777@gmail.com>
2018-09-21 06:00:38 +0600

    Search bar added.

--- view/estimatorslayoutbase.py
+++ view/estimatorslayoutbase.py
@@ -1,4 +1,6 @@
 from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
 
 from model import database
 from .miscellaneous import RV, represent_estimator_in_rv
@@ -8,7 +10,16 @@ class EstimatorsLayoutBase(RelativeLayout):
     def __init__(self, **kwargs):
         super(EstimatorsLayoutBase, self).__init__(**kwargs)
 
-        self.rv = RV()
+        n = 18
+        idx = 16
+        self.add_widget(Label(text='Search bar:', bold=True, italic=True, size_hint=(0.15, 1/n), pos_hint={'x': 0, 'y': 1/n*idx}))
+        self.text_input_search_title = TextInput(text='', hint_text='title like', password=False, multiline=False, write_tab=False, focus=False, size_hint=(0.40, 1/n), pos_hint={'x': 0.15, 'y': 1/n*idx})
+        self.add_widget(self.text_input_search_title)
+        self.text_input_search_type = TextInput(text='', hint_text='type like', password=False, multiline=False, write_tab=False, focus=False, size_hint=(0.40, 1/n), pos_hint={'x': 0.57, 'y': 1/n*idx})
+        self.add_widget(self.text_input_search_type)
+
+        idx -= 1
+        self.rv = RV(size_hint=(1, 1/n*idx), pos_hint={'x': 0, 'y': 0})
 
         db = database.DB()
         estimator_list = db.get_estimators()
@@ -19,10 +30,34 @@ class EstimatorsLayoutBase(RelativeLayout):
         self.rv.data = L
         self.add_widget(self.rv)
 
+        self.text_input_search_title.bind(text=self.search_bind)
+        self.text_input_search_type.bind(text=self.search_bind)
 
-    def reload_rv_data(self, *args):
-        self.db.create_new_session()
-        estimator_list = db.get_estimators()
+
+    def update_rv_data(self, estimator_list, *args):
         L = [{'text': 'Add new'}]
         L += [{'text': represent_estimator_in_rv(e)} for e in estimator_list]
         self.rv.data = L
+
+    def reload_rv_data(self, *args):
+        self.text_input_search_title.text = ''
+        self.text_input_search_type.text = ''
+
+        self.db.create_new_session()
+        estimator_list = self.db.get_estimators()
+        self.update_rv_data(estimator_list)
+
+
+    def search_bind(self, *args):
+        titlelike = '%' + self.text_input_search_title.text + '%'
+        typelike = '%' + self.text_input_search_type.text + '%'
+
+        qry = self.db.session.query(database.Estimator)
+
+        if len(titlelike) > 2:
+            qry = qry.filter(database.Estimator.title.like(titlelike))
+        if len(typelike) > 2:
+            qry = qry.filter(database.Estimator.type.like(typelike))
+
+        user_list = qry.all()
+        self.update_rv_data(user_list)
