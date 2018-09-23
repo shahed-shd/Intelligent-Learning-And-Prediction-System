39591fc3bd55f4d047193ca975376ec5d337e87c
Md. Shahedul Islam Shahed <shahed.shd777@gmail.com>
2018-09-24 03:29:17 +0600

    button to add new user is moved to upper left corner.

--- view/userslayout.py
+++ view/userslayout.py
@@ -1,11 +1,11 @@
 import functools
 
 from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.popup import Popup
 
-# from .userslayoutbase import UsersLayoutBase
 from model import database
 from .miscellaneous import represent_user_in_rv
 from .rv import RV
@@ -18,10 +18,12 @@ class UsersLayout(RelativeLayout):
 
         n = 18
         idx = 16
-        self.add_widget(Label(text='Search bar:', bold=True, italic=True, size_hint=(0.15, 1/n), pos_hint={'x': 0, 'y': 1/n*idx}))
-        self.text_input_search_username = TextInput(text='', hint_text='user name like', password=False, multiline=False, write_tab=False, focus=False, size_hint=(0.40, 1/n), pos_hint={'x': 0.15, 'y': 1/n*idx})
+        self.btn_add_new = Button(text='Add new user', italic=True, on_release=self.btn_add_new_do, size_hint=(0.20, 1/n), pos_hint={'x': 0.05, 'y': 1/n*idx})
+        self.add_widget(self.btn_add_new)
+        self.add_widget(Label(text='Search bar:', bold=True, italic=True, size_hint=(0.10, 1/n), pos_hint={'x': 0.45, 'y': 1/n*idx}))
+        self.text_input_search_username = TextInput(text='', hint_text='user name like', password=False, multiline=False, write_tab=False, focus=False, size_hint=(0.20, 1/n), pos_hint={'x': 0.55, 'y': 1/n*idx})
         self.add_widget(self.text_input_search_username)
-        self.text_input_search_fullname = TextInput(text='', hint_text='full name like', password=False, multiline=False, write_tab=False, focus=False, size_hint=(0.40, 1/n), pos_hint={'x': 0.57, 'y': 1/n*idx})
+        self.text_input_search_fullname = TextInput(text='', hint_text='full name like', password=False, multiline=False, write_tab=False, focus=False, size_hint=(0.20, 1/n), pos_hint={'x': 0.77, 'y': 1/n*idx})
         self.add_widget(self.text_input_search_fullname)
 
         idx -= 1
@@ -29,8 +31,8 @@ class UsersLayout(RelativeLayout):
 
         db = database.DB()
         user_list = db.get_users()
-        L = [{'text': 'Add new'}]
-        L += [{'text': represent_user_in_rv(u), 'on_release': functools.partial(self.show_user_profile_on_release, user_id=u.id)} for u in user_list]
+        # L = [{'text': 'Add new'}]
+        L = [{'text': represent_user_in_rv(u), 'on_release': functools.partial(self.show_user_profile_on_release, user_id=u.id)} for u in user_list]
 
         self.db = db
         self.rv.data = L
@@ -43,6 +45,10 @@ class UsersLayout(RelativeLayout):
         self.popup_user_profile.content.db = self.db
 
 
+    def btn_add_new_do(self, *args):
+        pass
+
+
     def show_user_profile_on_release(self, *args, **kwargs):
         user_id = kwargs['user_id']
         user = self.db.get_user_by_id(user_id)
@@ -52,8 +58,8 @@ class UsersLayout(RelativeLayout):
 
 
     def update_rv_data(self, user_list, *args):
-        L = [{'text': 'Add new'}]
-        L += [{'text': represent_user_in_rv(u), 'on_release': functools.partial(self.show_user_profile_on_release, user_id=u.id)} for u in user_list]
+        # L = [{'text': 'Add new'}]
+        L = [{'text': represent_user_in_rv(u), 'on_release': functools.partial(self.show_user_profile_on_release, user_id=u.id)} for u in user_list]
         self.rv.data = L
 
 
